03a2e22c513ffc3853e2a072dd5a2c44d47553b0
Marcelle Vukoja <booksread555@gmail.com>
2022-12-08 16:13:47 +1100

    Update css media queries

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-/* About */
+/* About -- displays the content in the active link */
 let aboutLinks = document.getElementsByClassName("about-links");
 let aboutLinksContent = document.getElementsByClassName("aboutlinks-content");
 
 for (let i = 0; i < aboutLinks.length; i++) {
     aboutLinks[i].addEventListener('click', function () {
-        for (j = 0; j < aboutLinks.length; j++) {
-            aboutLinks[j].className = 'about-links';
-            aboutLinksContent[j].className = "aboutlinks-content";
+        for (l = 0; l < aboutLinks.length; l++) {
+            aboutLinks[l].className = 'about-links';
+            aboutLinksContent[l].className = "aboutlinks-content";
         }
         this.classList.add('active-link');
         aboutLinksContent[i].className += ' active-tab';
     })
 }
+
